e0be0bc0d0dacde228a2a9fcc5ac0a794d4aa5c2
Jonathon Pinnow <pinnowj@gmail.com>
2022-12-11 00:57:35 -0500

    working solution for concurrent file duplicate detection

--- cmd/ingestor/main.go
+++ cmd/ingestor/main.go
@@ -1,6 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"sync"
+)
 
 /*
 Parallel Uniqueness Checker:
@@ -13,5 +21,74 @@ attachments of this email.
 */
 
 func main() {
+	/*
+		barcode,                        code,  YearWeek
+		TestProcessEligibleChannel2_0,  HFGK,  TestProcessEligibleChannel2_0
+	*/
 	fmt.Println("ingestor for Sparkfly")
+	// setup concurrency dependencies
+	ctx, cf := context.WithCancel(context.Background()) // used to drive short circuit behavior
+	wg := new(sync.WaitGroup)
+	codesFound := new(sync.Map)
+	dir, err := os.Open("./testdata")
+	if err != nil {
+		panic(err)
+	}
+	defer dir.Close()
+	files, err := dir.ReadDir(-1)
+	for _, f := range files {
+		file, err := os.Open(dir.Name() + "/" + f.Name())
+		defer file.Close()
+		if err != nil {
+			log.Printf("failed to open file: %s err: %s", f.Name(), err.Error())
+			continue
+		}
+		fmt.Printf("launch go routine for file: %s\n", file.Name())
+		wg.Add(1)
+		go readFile(ctx, cf, file, wg, codesFound)
+	}
+	wg.Wait()
+}
+
+func readFile(ctx context.Context, cf context.CancelFunc, f *os.File, wg *sync.WaitGroup, store *sync.Map) {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Printf("go routine paniced, file: %s error: %s", f.Name(), err)
+		}
+	}()
+	defer wg.Done()
+	reader := csv.NewReader(f)
+	reader.ReuseRecord = true
+	var readerErr error
+	var firstLineSkipped bool
+
+	for readerErr != io.EOF {
+		select {
+		case <-ctx.Done():
+			log.Println("aborting context finished")
+			return
+		default:
+			if !firstLineSkipped {
+				reader.Read()
+				firstLineSkipped = true
+				continue
+			}
+			line, err := reader.Read()
+			if err != nil && err != io.EOF {
+				log.Printf("aborting file: %s, could not read line: %s", f.Name(), err.Error())
+				cf()
+				return
+			}
+			if len(line) == 0 {
+				log.Printf("record contains no data at offset: %d", reader.InputOffset())
+				return
+			}
+			if _, ok := store.Load(line[1]); ok {
+				log.Printf("aborting, duplicate found: %s", line[1])
+				cf()
+				return
+			}
+			store.Store(line[1], struct{}{})
+		}
+	}
 }
